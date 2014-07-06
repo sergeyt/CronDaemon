6c4544aaa0736b7e2b7bdc325afbe7364c6e8eac
sergeyt <stodyshev@gmail.com>
2014-07-07 00:28:22 +0700

    cron expression builder and tests

---
+++ src/CronDaemon/Cron.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using NCrontab;
+
+namespace CronScheduling
+{
+	/// <summary>
+	/// Typed wrapper for Cron expressions.
+	/// </summary>
+	public struct CronExpression
+	{
+		public readonly string Expression;
+
+		public CronExpression(string expression)
+		{
+			Expression = expression;
+		}
+
+		public string ToHumanString()
+		{
+			return CrontabSchedule.Parse(Expression).ToString();
+		}
+
+		public static implicit operator CronExpression(string expression)
+		{
+			return new CronExpression(expression);
+		}
+
+		public static implicit operator string(CronExpression expression)
+		{
+			return expression.Expression;
+		}
+	}
+
+	/// <summary>
+	/// Cron expression builder.
+	/// </summary>
+	public static class Cron
+	{
+		/// <summary>
+		/// Every minute.
+		/// </summary>
+		public static CronExpression Minutely()
+		{
+			return "* * * * *";
+		}
+
+		/// <summary>
+		/// Every hour at the specified minute.
+		/// </summary>
+		public static CronExpression Hourly(int minute = 0)
+		{
+			return String.Format("{0} * * * *", minute);
+		}
+
+		/// <summary>
+		/// Every day at the specified hour and minute.
+		/// </summary>
+		public static CronExpression Daily(int hour = 0, int minute = 0)
+		{
+			return String.Format("{0} {1} * * *", minute, hour);
+		}
+
+		/// <summary>
+		/// Every week at the specified day of week, hour and minute.
+		/// </summary>
+		public static CronExpression Weekly(DayOfWeek dayOfWeek = DayOfWeek.Monday, int hour = 0, int minute = 0)
+		{
+			return String.Format("{0} {1} * * {2}", minute, hour, (int)dayOfWeek);
+		}
+
+		/// <summary>
+		/// Every month at the specified day of month, hour and minute.
+		/// </summary>
+		public static CronExpression Monthly(int day = 1, int hour = 0, int minute = 0)
+		{
+			return String.Format("{0} {1} {2} * *", minute, hour, day);
+		}
+
+		/// <summary>
+		/// Every year at the specified month, day, hour and minute.
+		/// </summary>
+		public static CronExpression Yearly(int month = 1, int day = 1, int hour = 0, int minute = 0)
+		{
+			return String.Format("{0} {1} {2} {3} *", minute, hour, day, month);
+		}
+	}
+}
--- src/CronDaemon/Tests/CronDaemonTests.cs
+++ src/CronDaemon/Tests/CronDaemonTests.cs
@@ -1,5 +1,6 @@
 ﻿#if NUNIT
 using System;
+using System.Threading;
 using NUnit.Framework;
 
 namespace CronScheduling.Tests
@@ -12,6 +13,85 @@ namespace CronScheduling.Tests
 		{
 			Assert.That(() => new CronDaemon<int>(null, null), Throws.InstanceOf<ArgumentNullException>());
 			Assert.That(() => new CronDaemon<int>(_ => { }, null), Throws.InstanceOf<ArgumentNullException>());
+		}
+
+		public static IDisposable WithShiftedOneMinuteTick()
+		{
+			var now = DateTime.UtcNow;
+			var delay = TimeSpan.FromSeconds(1);
+			now = now.Subtract(TimeSpan.FromSeconds(now.Second).Add(TimeSpan.FromMilliseconds(now.Millisecond))).Subtract(delay);
+			var timeIndex = 0;
+
+			SystemTime.NowFn = () =>
+			{
+				if (timeIndex == 0)
+				{
+					++timeIndex;
+					return now;
+				}
+
+				var t = now;
+				now = now.Add(TimeSpan.FromMinutes(1));
+				return t;
+			};
+
+			return new DisposableAction(() => SystemTime.NowFn = null);
+		}
+
+		[Test]
+		public void RunForever()
+		{
+			using (WithShiftedOneMinuteTick())
+			{
+				var count = 0;
+				var done = new ManualResetEvent(false);
+				var crond = new CronDaemon<int>(
+					i =>
+					{
+						count++;
+						if (count >= 3)
+						{
+							done.Set();
+						}
+					},
+					i => i
+					);
+
+				using (crond)
+				{
+					crond.Add(1, Cron.Minutely(), int.MaxValue);
+
+					Assert.IsTrue(done.WaitOne(TimeSpan.FromSeconds(3)));
+				}
+			}
+		}
+
+		// TODO make it faster
+		[TestCase(0, Result = 1)]
+		[TestCase(1, Result = 2)]
+		public int RunTimes(int times)
+		{
+			using (WithShiftedOneMinuteTick())
+			{
+				var count = 0;
+				var done = new ManualResetEvent(false);
+				var crond = new CronDaemon<int>(
+					i =>
+					{
+						count++;
+					},
+					i => i
+					);
+
+				using (crond)
+				{
+					crond.Add(1, Cron.Minutely(), times);
+
+					done.WaitOne(TimeSpan.FromSeconds(3));
+				}
+
+				return count;
+			}
 		}
 	}
 }
